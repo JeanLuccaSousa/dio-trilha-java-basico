c6c7cd5b3cd8478d1bf6252eb157e360f54bf77b
Jean <jean_luccalp@hotmail.com>
2023-03-02 18:30:00 +0100

    Operadores

---
+++ tipos-variaveis/src/Operadores.java
@@ -0,0 +1,7 @@
+public class Operadores {
+    public static void main(String[] args) {
+        String nomeCurso = " Linguagem " + " Java ";
+
+        System.out.println(nomeCurso);
+    }
+}
--- tipos-variaveis/src/TiposVariaveis.java
+++ tipos-variaveis/src/TiposVariaveis.java
@@ -1,5 +1,13 @@
-public class App {
+public class TiposVariaveis {
     public static void main(String[] args) throws Exception {
-        System.out.println("Hello, World!");
+       
+        //final int NUMERO = 20;
+
+        //NUMERO = 14;
+
+        double multi = (8 * 5) + (40 / 2);
+
+        System.out.println(multi);
+
     }
 }
